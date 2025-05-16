247b6266c84c9cc82ba37263c152ad361c9f1b5b
Gabriel Rodegheri <engenheirogabrielrodegheri@gmail.com>
2025-05-16 17:50:54 -0300

    Jarvis respondendo pelo nome.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 while True:
     comando = input('Como posso ajudá-lo hoje? ')
     if comando == 'sair':
+        print('Até a próxima!!')
         break
-    print(f'Você disse: {comando}')
+    if comando.lower()[0:6] != 'jarvis':
+        print('Você não me chamou pelo meu nome. És um impostor? xD')
+        continue
+    else:
+        print(f'Estou ouvindo! Você disse: {comando}')
+    
